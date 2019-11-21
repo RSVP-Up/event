9a5f08787d254c787d32f83093d61bb96daf8b45
Manuela Gomez <mmanu.gomez@gmail.com>
2019-11-20 23:46:27 -0500

    [WIP] Write a helper function to generate random org members

--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
-// Organization model in MongoDB
-// Org =
+// Group model in MongoDB
+// Group =
 // {
 //  orgId: int,
 //  name: string,
@@ -30,13 +30,14 @@ const faker = require('faker')
 
 // There are usually less organizations than there are events
 // Or, in other words, each organization can hold multiple events, of which there are 100
+// With 20 events distributed evenly that is an average of 5 events per org
 let organizations = [];
 
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 20; i++) {
   const orgId = 'o' + i;
   const name = faker.company.companyName();
   const private = faker.random.boolean();
-  const members;
+  const members = eventMembers();
   const events;
 
   let newOrg = {
@@ -50,4 +51,26 @@ for (let i = 0; i < 30; i++) {
   organizations.push(newOrg);
 }
 
-//
+//Each organization has between 1 and 4 organizers and up to 50 members
+var eventMembers = () => {
+  const organizers = memberIds(4);
+  const groupMembers = memberIds(50);
+
+  let members = {
+    organizers,
+    groupMembers
+  };
+
+  return members;
+}
+
+// Given a maxiumum quantity max, returns an array of memberIds between 1 and max
+var memberIds = (max) => {
+  let ids = [];
+  const numberOfElements = faker.random.number({min: 1, max: max});
+  for (let i = 0; i < numberOfElements; i ++) {
+    const newId = 'm' + faker.random.number(499);
+    ids.push(newId);
+  }
+  return ids;
+}
