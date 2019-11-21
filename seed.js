bf627e7ce052d1b62bf9b8b4609b62609bb9a512
Manuela Gomez <mmanu.gomez@gmail.com>
2019-11-21 00:54:21 -0500

    [WIP] Distribute all 100 events in different orgs, 5 per org

--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
-// Group model in MongoDB
-// Group =
+// Org model in MongoDB
+// Org =
 // {
 //  orgId: int,
 //  name: string,
@@ -8,25 +8,23 @@
 //    organizers: [ memberId: “m” + int, …],
 //    groupMembers: [ memberId: “m” + int , …],
 //  },
-//  events: [
-//    {
+//  events: [{
 //     eventId: int,
 //     title: string,
 //     local_date_time: ISO 8601,
 //     series: {
 //       description: string,
 //       frequency: {
-//         days_of_week: string,
+//         day_of_week: string,
 //         interval: int,
 //         week_of_month: int
-//         },
 //         endDate: ISO 8601,
-//       },
-//     }
-//   ],
+//        },
+//      },
+//   }...],
 // }
 
-const faker = require('faker')
+const faker = require('faker');
 
 // There are usually less organizations than there are events
 // Or, in other words, each organization can hold multiple events, of which there are 100
@@ -38,14 +36,14 @@ for (let i = 0; i < 20; i++) {
   const name = faker.company.companyName();
   const private = faker.random.boolean();
   const members = eventMembers();
-  const events;
+  const events = orgEvents(i);
 
   let newOrg = {
     orgId,
     name,
     private,
     members,
-    events
+    events,
   }
 
   organizations.push(newOrg);
@@ -58,13 +56,13 @@ var eventMembers = () => {
 
   let members = {
     organizers,
-    groupMembers
+    groupMembers,
   };
 
   return members;
 }
 
-// Given a maxiumum quantity max, returns an array of memberIds between 1 and max
+// Given a maximum quantity max, returns an array of memberIds between 1 and max
 var memberIds = (max) => {
   let ids = [];
   const numberOfElements = faker.random.number({min: 1, max: max});
@@ -73,4 +71,44 @@ var memberIds = (max) => {
     ids.push(newId);
   }
   return ids;
+}
+
+var orgEvents = (range) => {
+  let events = [];
+  for (let i = 0; i < 5; i++) {
+  const eventId = range * 5 + i;
+  const title = faker.random.catchPhrase();
+  const local_date_time = faker.date.between('2019-10-01', '2020-9-30');
+  const series = eventSeries();
+
+  let newEvent = {
+    eventId,
+    title,
+    local_date_time,
+    series
+  }
+
+  events.push(newEvent);
+
+  }
+
+  return events;
+}
+
+// some, but not all events might repeat
+var eventSeries = () => {
+  const repeat = faker.random.boolean();
+  const ordinals = ['1st', '2nd', '3rd'];
+  const frequency = {
+    day_of_week: faker.date.weekday(),
+    interval: faker.random.arrayElement(ordinals);
+    endDate: faker.date.future(1),
+  }
+  const description = 'Every ' + frequency.interval + ' ' + frequency.day_of_week + ' of the month until May 2020';
+
+  let newSeries = {
+    description,
+    frequency,
+  };
+  return repeat ? newSeries : null;
 }
