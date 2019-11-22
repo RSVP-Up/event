50804d141ce600e4ce8288e55bd6691c0f5a96f2
Manuela Gomez <mmanu.gomez@gmail.com>
2019-11-22 01:34:22 -0500

    [wip] Passing linter and syntax check

--- database/seed.js
+++ database/seed.js
@@ -1,12 +1,14 @@
+/* eslint-disable camelcase */
+/* eslint-disable prefer-const */
 // Org model in MongoDB
 // Org =
 // {
 //  orgId: int,
-//  name: string,
-//  private: boolean,
+//  org_name: string,
+//  org_private: boolean,
 //  members: {
 //    organizers: [ memberId: “m” + int, …],
-//    groupMembers: [ memberId: “m” + int , …],
+//    group_members: [ memberId: “m” + int , …],
 //  },
 //  events: [{
 //     eventId: int,
@@ -18,7 +20,6 @@
 //         day_of_week: string,
 //         interval: int,
 //         week_of_month: int
-//         endDate: ISO 8601,
 //        },
 //      },
 //   }...],
@@ -29,85 +30,83 @@ const faker = require('faker');
 // There are usually less organizations than there are events
 // Or, in other words, each organization can hold multiple events, of which there are 100
 // With 20 events distributed evenly that is an average of 5 events per org
-let organizations = [];
 
-for (let i = 0; i < 20; i++) {
-  const orgId = 'o' + i;
-  const name = faker.company.companyName();
-  const private = faker.random.boolean();
-  const members = eventMembers();
-  const events = orgEvents(i);
-
-  let newOrg = {
-    orgId,
-    name,
-    private,
-    members,
-    events,
-  }
-
-  organizations.push(newOrg);
-}
-
-//Each organization has between 1 and 4 organizers and up to 50 members
-var eventMembers = () => {
-  const organizers = memberIds(4);
-  const groupMembers = memberIds(50);
-
-  let members = {
-    organizers,
-    groupMembers,
-  };
-
-  return members;
-}
-
-// Given a maximum quantity max, returns an array of memberIds between 1 and max
-var memberIds = (max) => {
-  let ids = [];
-  const numberOfElements = faker.random.number({min: 1, max: max});
-  for (let i = 0; i < numberOfElements; i ++) {
-    const newId = 'm' + faker.random.number(499);
+const memberIds = (max) => {
+  const ids = [];
+  const numberOfElements = faker.random.number({ min: 1, max });
+  for (let i = 0; i < numberOfElements; i += 1) {
+    const newId = `m${faker.random.number(499)}`;
     ids.push(newId);
   }
   return ids;
-}
-
-var orgEvents = (range) => {
-  let events = [];
-
-  for (let i = 0; i < 5; i++) {
-  const eventId = range * 5 + i;
-  const title = faker.random.catchPhrase();
-  const local_date_time = faker.date.between('2019-10-01', '2020-4-30');
-  const series = eventSeries();
-
-  let newEvent = {
-    eventId,
-    title,
-    local_date_time,
-    series
-  }
-
-  events.push(newEvent);
-  }
-
-  return events;
-}
-
+};
+// Each organization has between 1 and 4 organizers and up to 50 members
+let eventMembers = () => {
+  const organizers = memberIds(4);
+  const group_members = memberIds(50);
+  const members = {
+    organizers,
+    group_members,
+  };
+  return members;
+};
 // some, but not all events might repeat
-var eventSeries = () => {
+let eventSeries = () => {
   const repeat = faker.random.boolean();
   const ordinals = ['1st', '2nd', '3rd'];
   const frequency = {
     day_of_week: faker.date.weekday(),
-    interval: faker.random.arrayElement(ordinals);
-  }
-  const description = 'Every ' + frequency.interval + ' ' + frequency.day_of_week + ' of the month until May 2020';
-
-  let newSeries = {
+    interval: faker.random.number(2),
+  };
+  const description = `Every ${ordinals[frequency.interval]} ${frequency.day_of_week} of the month until May 2020`;
+  const newSeries = {
     description,
     frequency,
   };
   return repeat ? newSeries : null;
+};
+
+// Given a maximum quantity max, returns an array of memberIds between 1 and max
+let orgEvents = (range) => {
+  const events = [];
+  for (let i = 0; i < 5; i += 1) {
+    const eventId = range * 5 + i;
+    const title = faker.company.catchPhrase();
+    const local_date_time = faker.date.between('2019-10-01', '2020-4-30');
+    const series = eventSeries();
+    const newEvent = {
+      eventId,
+      title,
+      local_date_time,
+      series,
+    };
+    events.push(newEvent);
+  }
+  return events;
+};
+
+const organizations = [];
+
+for (let i = 0; i < 20; i += 1) {
+  const orgId = `o${i}`;
+  const org_name = faker.company.companyName();
+  const org_private = faker.random.boolean();
+  const members = eventMembers();
+  const events = orgEvents(i);
+
+  const newOrg = {
+    orgId,
+    org_name,
+    org_private,
+    members,
+    events,
+  };
+
+  organizations.push(newOrg);
 }
+
+module.exports.organization = organizations;
+module.exports.eventMembers = eventMembers;
+module.exports.memberIds = memberIds;
+module.exports.orgEvents = orgEvents;
+module.exports.eventSeries = eventSeries;
