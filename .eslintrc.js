b95d4d46afd749497dfb849f4538a48a279de121
Manuela Gomez <mmanu.gomez@gmail.com>
2019-11-21 23:00:56 -0500

    (init)

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    /* Indentation */
+    'no-mixed-spaces-and-tabs': 2,
+    'indent': [2, 2],
+    /* Variable cames */
+    'camelcase': 2,
+    /* Language constructs */
+    'curly': 2,
+    'eqeqeq': [2, 'smart'],
+    'func-style': [2, 'expression'],
+    /* Semicolons */
+    'semi': 2,
+    'no-extra-semi': 2,
+    /* Padding & additional whitespace (perferred but optional) */
+    'brace-style': [2, '1tbs', { 'allowSingleLine': true }],
+    'semi-spacing': 1,
+    'key-spacing': 1,
+    'block-spacing': 1,
+    'comma-spacing': 1,
+    'no-multi-spaces': 1,
+    'space-before-blocks': 1,
+    'keyword-spacing': [1, { 'before': true, 'after': true }],
+    'space-infix-ops': 1,
+    /* Variable declaration */
+    'one-var': [1, { 'uninitialized': 'always', 'initialized': 'never' }],
+    /* Minuta */
+    'comma-style': [2, 'last'],
+    'quotes': [1, 'single']
+  },
+};
