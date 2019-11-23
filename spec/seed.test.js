54529a434c0ee35cfec247f42aacc35d643eeeda
Manuela Gomez <mmanu.gomez@gmail.com>
2019-11-23 16:57:39 -0500

    [WIP] Added test to check for eventIds

--- spec/seed.test.js
+++ spec/seed.test.js
@@ -1,15 +1,21 @@
-const organizations = require('../database/seed').organizations;
-const events = require('../database/seed.js').events;
-const expect = require('chai').expect;
+const { expect } = require('chai');
+const { organizations } = require('../database/seed');
+const { events } = require('../database/seed.js');
 
 describe('Events', () => {
-  test('to account for 100 main records', () => {
+  test('Should account for 100 main records', () => {
     expect(events).to.have.lengthOf(100);
+  });
+
+  test('Each event should have an id', () => {
+    const eventIdx = Math.floor(Math.random() * 100);
+    const event = events[eventIdx];
+    expect(event).to.have.property('eventId');
   });
 });
 
 describe('Organizations', () => {
-  test('to be less than the total events', () => {
+  test('Should be less than the total events', () => {
     expect(organizations.length).to.be.below(100);
   });
 });
