90a2c1b9b9975010bd7a4b67659e56ae416588e5
Manuela Gomez <mmanu.gomez@gmail.com>
2019-11-23 18:27:49 -0500

    [wip] Added test for organization schema

--- spec/seed.test.js
+++ spec/seed.test.js
@@ -6,16 +6,40 @@ describe('Events', () => {
   test('Should account for 100 main records', () => {
     expect(events).to.have.lengthOf(100);
   });
-
-  test('Each event should have an id', () => {
-    const eventIdx = Math.floor(Math.random() * 100);
-    const event = events[eventIdx];
-    expect(event).to.have.property('eventId');
+  describe('Schema', () => {
+    let event;
+    beforeEach(() => {
+      const eventIdx = Math.floor(Math.random() * 100);
+      event = events[eventIdx];
+    });
+    test('Each event should be an object', () => {
+      expect(event).to.be.an('object');
+    });
+    test('Each event should store eventId, title, date, orgId, and series', () => {
+      const keys = ['eventId', 'title', 'local_date_time', 'orgId', 'series'];
+      expect(event).to.have.all.keys(keys);
+    });
   });
+  // test('Not all events repeat', () => {
+  // });
 });
 
 describe('Organizations', () => {
   test('Should be less than the total events', () => {
     expect(organizations.length).to.be.below(100);
+  });
+  describe('Schema', () => {
+    let org;
+    beforeEach(() => {
+      const orgIdx = Math.floor(Math.random() * 20);
+      org = organizations[orgIdx];
+    });
+    test('Each organization should be an object', () => {
+      expect(org).to.be.an('object');
+    });
+    test('Each organization should store orgId and org_name, whether it\'s private, and members', () => {
+      const keys = ['orgId', 'org_name', 'org_private', 'members'];
+      expect(org).to.have.all.keys(keys);
+    });
   });
 });
