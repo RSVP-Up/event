d45ef93dd424a7eefab6075685e02b7ba6a34269
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-03 22:33:28 -0500

    [wip] Org schema added for model

---
+++ database/Org.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = require('bluebird');
+
+const { Schema } = mongoose;
+
+const orgSchema = new Schema({
+  orgId: String,
+  org_name: String,
+  org_private: Boolean,
+  members: [String],
+}, { _id: false });
+
+const Org = mongoose.model('Org', orgSchema);
+
+module.exports = Org;
