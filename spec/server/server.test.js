7533981611ba0c6c0dea3f8864c4c77ec831e529
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-08 15:46:03 -0500

    [WIP] Wrote 1 test for all endpoints

--- spec/server/server.test.js
+++ spec/server/server.test.js
@@ -1,12 +1,29 @@
-const _ = require('lodash');
-const { expect }= require('chai');
-const server = 'http://localhost:5000';
+// const _ = require('lodash');
+const { expect } = require('chai');
 const sinon = require('sinon');
 const request = require('request');
 
-describe('Event module', () => {
+const server = 'http://localhost:5000';
+
+describe('Event API', () => {
+  describe('GET /event/summary/:eventId', () => {
+    test('Should return event data necessary to render the Event module: event title, name of the organization hosting it, and whether that organization is public or private', (done) => {
+      const eventDataKeys = ['title', 'org_name', 'org_private'];
+      const randomEventId = Math.floor(Math.random() * 100);
+      request.get(`${server}/event/summary/${randomEventId}`, (err, res, body) => {
+        // make sure the resonse contains a status code and that it's 200 since it's successful
+        expect(res.statusCode).to.equal(200);
+        // the response should be JSON
+        expect(res.headers['content-type']).to.contain('application/json');
+        // convert the JSON response
+        const parsedBody = JSON.parse(body);
+        expect(parsedBody).to.include.all.keys(eventDataKeys);
+        done();
+      });
+    });
+  });
   describe('GET /event/:eventId', () => {
-    test('Should return event data necessary to render the Event module: event title, date and time, name of the organization hosting it, and whether that organization is public or private', (done) => {
+    test('Should return the date and time as well as the summary of the event', (done) => {
       const eventDataKeys = ['title', 'local_date_time', 'org_name', 'org_private'];
       const randomEventId = Math.floor(Math.random() * 100);
       request.get(`${server}/event/${randomEventId}`, (err, res, body) => {
@@ -15,8 +32,40 @@ describe('Event module', () => {
         // the response should be JSON
         expect(res.headers['content-type']).to.contain('application/json');
         // convert the JSON response
-        let parsedBody = JSON.parse(body);
+        const parsedBody = JSON.parse(body);
         expect(parsedBody).to.include.all.keys(eventDataKeys);
+        done();
+      });
+    });
+  });
+  describe('GET /event/org/members/:eventId', () => {
+    test('Should return the organization members and founders', (done) => {
+      const memberDataKeys = ['founders', 'group_members'];
+      const randomEventId = Math.floor(Math.random() * 100);
+      request.get(`${server}/event/org/members/${randomEventId}`, (err, res, body) => {
+        // make sure the resonse contains a status code and that it's 200 since it's successful
+        expect(res.statusCode).to.equal(200);
+        // the response should be JSON
+        expect(res.headers['content-type']).to.contain('application/json');
+        // convert the JSON response
+        const parsedBody = JSON.parse(body);
+        expect(parsedBody).to.include.all.keys(memberDataKeys);
+        done();
+      });
+    });
+  });
+  describe('GET /event/timedate/:eventId', () => {
+    test('Should return the time and date of the event and its frequency', (done) => {
+      const timeDateDataKeys = ['local_date_time', 'description'];
+      const randomEventId = Math.floor(Math.random() * 100);
+      request.get(`${server}/event/timedate/${randomEventId}`, (err, res, body) => {
+        // make sure the resonse contains a status code and that it's 200 since it's successful
+        expect(res.statusCode).to.equal(200);
+        // the response should be JSON
+        expect(res.headers['content-type']).to.contain('application/json');
+        // convert the JSON response
+        const parsedBody = JSON.parse(body);
+        expect(parsedBody).to.include.all.keys(timeDateDataKeys);
         done();
       });
     });
