af084b85f3ab36e0e47ace196fb38d2e2b8c0e8d
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-09 09:08:21 -0500

    [WIP] Webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '/client/index.jsx'),
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/public'),
+  },
+};
