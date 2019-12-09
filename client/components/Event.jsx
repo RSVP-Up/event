7aea8dc0220c35b121a8a25765d94cdb339009d1
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-09 09:07:14 -0500

    [WIP] Added source client file setup

---
+++ client/components/Event.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Event extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return <div>Event component</div>;
+  }
+}
+
+export default Event;
---
+++ client/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// TO Do install react and react dom
+// install babel and webpack
+
+import Event from './components/Event';
+
+ReactDOM.render(<Event />, document.getElementById('event'));
