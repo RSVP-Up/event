9254bf4824d7346b0994d8bf2296aa9740fc28fe
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-10 21:51:52 -0500

    [WIP] removed comments

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// TO Do install react and react dom
-// install babel and webpack
-
 import Event from './components/Event';
 
 ReactDOM.render(<Event />, document.getElementById('event'));
