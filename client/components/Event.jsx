2722259d5ad62330d0788af941438f0ef88fc4f1
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-16 10:19:30 -0500

    [WIP] Event component renders to look closely like meetup event header using semantic ui components

--- client/components/Event.jsx
+++ client/components/Event.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import moment from 'moment';
+import {
+  Button,
+  Divider,
+  Item,
+  Icon,
+  Grid,
+  Container,
+} from 'semantic-ui-react';
+import style from '../styles';
+import Hosts from './hosts';
 
-const eventAPI = 'http://localhost:5000/event';
+// const eventAPI = 'http://localhost:5000/event';
 // const eventId = randomeventId
 
 class Event extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
   }
 
   // fetchEventData() {
@@ -19,10 +27,43 @@ class Event extends React.Component {
   // }
 
   render() {
+    const {
+      data: {
+        event: { title, local_date_time },
+        hosts,
+      },
+    } = this.props;
+    const date = moment(local_date_time).format('LL');
+    const weekday = moment(local_date_time).format('dddd');
+    const timeDate = `${weekday}, ${date}`;
     return (
-      <div>
-        Event component
-        <Button>Click Here</Button>
+      <div style={style.div}>
+        <Container text>
+          <Item style={style.item}>
+            <Item.Content verticalAlign="middle">
+              <Item.Meta>
+                <p style={style.timeDate}>{timeDate}</p>
+              </Item.Meta>
+              <Item.Header style={style.title}>
+                {title}
+              </Item.Header>
+              <Item.Description>
+                <Grid columns={2} stackable>
+                  <Grid.Column floated="left">
+                    <Hosts style={style} hosts={hosts} />
+                  </Grid.Column>
+                  <Grid.Column verticalAlign="bottom" floated="right">
+                    <Button size="medium" basic floated="right" style={style.button}>
+                      <Icon name="share square" />
+                      Share
+                    </Button>
+                  </Grid.Column>
+                </Grid>
+              </Item.Description>
+            </Item.Content>
+          </Item>
+        </Container>
+        <Divider />
       </div>
     );
   }
