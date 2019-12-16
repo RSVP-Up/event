1491b18d38d7b926f2c56eb6562198c80e8ea988
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-16 10:15:10 -0500

    Updated from example gist for rsvp service

--- client/sample_data.js
+++ client/sample_data.js
@@ -3,7 +3,7 @@ const event = {
   title: 'Grass-roots web-enabled info-mediaries',
   local_date_time: '2019-10-07T08:05:25.286Z',
   org_name: 'Koch, Blick and Turcotte',
-  private: true
+  private: true,
 };
 
 // GET event hosts
