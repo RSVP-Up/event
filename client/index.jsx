f01d489a6244404a8d9bdd2ce80a8d27873bfe17
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-16 10:16:39 -0500

    [WIP] importing sample data and serving as props to event, importing semantic-ui-react to use components

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 
 import Event from './components/Event';
+import data from './sample_data';
 
-ReactDOM.render(<Event />, document.getElementById('event'));
+ReactDOM.render(<Event data={data} />, document.getElementById('event'));
