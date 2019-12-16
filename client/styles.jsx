bf0b5c2ab509a06533d4df047390a5c5200f695a
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-16 10:15:50 -0500

    [WIP] custom styles for meetup page feel

---
+++ client/styles.jsx
@@ -0,0 +1,38 @@
+const style = {
+  timeDate: {
+    color: '#757575',
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: '14px',
+    fontWeight: '200',
+    margin: '14px 0px 0px 0px',
+  },
+  button: {
+    borderRadius: '8px',
+    padding: '10px 20px',
+    fontSize: '12px',
+    fontFamily: '"Roboto", sans-serif',
+  },
+  title: {
+    fontFamily: '"Roboto", sans-serif',
+    color: '#212121',
+    fontSize: '28px',
+    fontWeight: '900',
+    margin: '0px 0px 8px 0px',
+  },
+  thumbnail: {
+    width: '40px',
+    height: '40px',
+    fontFamily: '\'Roboto\', sans-serif',
+  },
+  item: {
+    boxSizing: 'border-box',
+    margin: 'auto',
+    display: 'block',
+  },
+  hostedBy: {
+    color: '#212121',
+  },
+
+};
+
+export default style;
