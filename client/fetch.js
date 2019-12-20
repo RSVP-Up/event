8800cae670ab938bb2dc9474a409878beb5572b9
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-19 23:14:12 -0500

    [WIP] using correct endpoint and reading body response with promise resolve

--- client/fetch.js
+++ client/fetch.js
@@ -1,15 +1,33 @@
 const eventAPI = 'http://localhost:5000/event/';
-const rsvpAPI = 'http://localhost:3001/rsvp/';
+const rsvpAPI = 'http://localhost:3001/rsvp/hosts/';
 
-const fetchEvent = (eventId) => (fetch(eventAPI + eventId));
+const fetchEvent = (eventId) => {
+  return fetch(eventAPI + eventId)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    });
+};
 
-const fetchHosts = (eventId) => (fetch(rsvpAPI + eventId, { mode: 'no-cors' }));
+const fetchHosts = (eventId) => {
+  return fetch(rsvpAPI + eventId)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    });
+};
 
 const fetchAllEventData = (eventId) => (
   Promise.all([
     fetchEvent(eventId),
     fetchHosts(eventId),
-  ]).then(([event, hosts]) => ({ event, hosts }))
-);
+  ]).then(([event, hosts]) => {
+    return { event, hosts };
+  }));
+
 
 export default fetchAllEventData;
