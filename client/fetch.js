d2bfaa619d8f5ffaf98ca58a44fc4e2b64e8cc29
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-21 16:13:22 -0500

    [WIP] using only database unique to service and sample data for rspv service

--- client/fetch.js
+++ client/fetch.js
@@ -1,5 +1,8 @@
+import data from './sample_data';
 const eventAPI = 'http://localhost:5000/event/';
-const rsvpAPI = 'http://localhost:3001/rsvp/hosts/';
+// const rsvpAPI = 'http://localhost:3001/rsvp/hosts/';
+
+const { hosts } = data;
 
 const fetchEvent = (eventId) => {
   return fetch(eventAPI + eventId)
@@ -11,23 +14,31 @@ const fetchEvent = (eventId) => {
     });
 };
 
-const fetchHosts = (eventId) => {
-  return fetch(rsvpAPI + eventId)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      return data;
-    });
-};
+// const fetchHosts = (eventId) => {
+//   return fetch(rsvpAPI + eventId)
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(data => {
+//       return data;
+//     });
+// };
 
-const fetchAllEventData = (eventId) => (
-  Promise.all([
-    fetchEvent(eventId),
-    fetchHosts(eventId),
-  ]).then(([event, hosts]) => {
-    return { event, hosts };
-  }));
+// const fetchAllEventData = (eventId) => (
+//   Promise.all([
+//     fetchEvent(eventId),
+//     fetchHosts(eventId),
+//   ]).then(([event, hosts]) => {
+//     return { event, hosts };
+//   }));
+
+const fetchAllEventData = (eventId) => {
+  return fetchEvent(eventId)
+    .then(event => {
+      console.log(event)
+      return { event, hosts };
+    })
+};
 
 
 export default fetchAllEventData;
