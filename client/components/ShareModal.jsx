3dece249b224df16acf60fe03897b1cb8a63fa46
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-06 21:10:50 -0500

    [WIP] refactored ShareModel to class component

--- client/components/ShareModal.jsx
+++ client/components/ShareModal.jsx
@@ -30,37 +30,49 @@ const shareTo = [
   },
 ];
 
-const ShareModal = ({ style, button }) => (
-  <Modal
-    trigger={(
-      <Button size="medium" basic floated="right" style={button}>
-        <Icon name="share square" />
-        Share
-      </Button>
-    )}
-    centered={false}
-    style={style}
-  >
-    <Modal.Content>
-      {/* TO DO create a click event on the image to close modal */}
-      <Button basic size="large" icon="times" />
-      <Container>
-        <Header as="h1" content="Share this event" />
-        <Grid style={{ fontFamily: '"Roboto", sans-serif' }} divided="vertically" verticalAlign="bottom">
-          {shareTo.map((destination) => (
-            <Grid.Row key={destination.content}>
-              <Container>
-                <Icon size="big" name={destination.iconName} color="grey" style={{ marginLeft: '10px' }} />
-                <a href="#" style={{ color: 'rgb(0, 162, 199)', cursor: 'pointer', fontSize: '16px' }}>
-                  {destination.content}
-                </a>
-              </Container>
-            </Grid.Row>
-          ))}
-        </Grid>
-      </Container>
-    </Modal.Content>
-  </Modal>
-);
+class ShareModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    const { style, button } = this.props;
+    return (
+      <Modal
+        trigger={(
+          <Button size="medium" basic floated="right" style={button}>
+            <Icon name="share square" />
+            Share
+          </Button>
+        )}
+        centered={false}
+        style={style}
+      >
+        <Modal.Content>
+          {/* TO DO create a click event on the image to close modal */}
+          <Button basic size="large" icon="times" />
+          <Container>
+            <Header as="h1" content="Share this event" />
+            <Grid style={{ fontFamily: '"Roboto", sans-serif' }} divided="vertically" verticalAlign="bottom">
+              {shareTo.map((destination) => (
+                <Grid.Row key={destination.content}>
+                  <Container>
+                    <Icon size="big" name={destination.iconName} color="grey" style={{ marginLeft: '10px' }} />
+                    <a href="#" style={{ color: 'rgb(0, 162, 199)', cursor: 'pointer', fontSize: '16px' }}>
+                      {destination.content}
+                    </a>
+                  </Container>
+                </Grid.Row>
+              ))}
+            </Grid>
+          </Container>
+        </Modal.Content>
+      </Modal>
+    );
+  }
+}
 
 export default ShareModal;
